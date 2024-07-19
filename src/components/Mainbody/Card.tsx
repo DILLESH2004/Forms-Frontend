1937d88cba57782cbafac09c06e84b6f9dbb1903
Bhagya Dillesh <109259289+DILLESH2004@users.noreply.github.com>
2024-07-19 14:46:05 +0530

    Update Card.tsx

--- src/components/Mainbody/Card.tsx
+++ src/components/Mainbody/Card.tsx
@@ -2,14 +2,12 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import MoreVertSharpIcon from '@mui/icons-material/MoreVertSharp';
 import StorageSharpIcon from '@mui/icons-material/StorageSharp';
-import formimage from "../../assets/images/t-shirt.png";
 import "./Mainbody.scss"
 
 export const Card: React.FC<any> = ({ document, openForm }) => {
   return <div className="doc-card" onClick={() => {
     openForm(document._id);
   }}>
-    <img src={formimage} alt="no-image" className="doc-image"></img>
     <div className="doc-content">
       <div className="doc-info">
         <p className="doc-name">{document.documentName}</p>
